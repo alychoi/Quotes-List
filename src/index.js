4e286f0637367cac698406476296248ee5eb3d80
skyblue0123 <alyssa.seoyoon@gmail.com>
2020-12-21 01:03:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,16 +29,11 @@ const Post = (props) => {
 const PostList = (props) => {
   
   console.log(props.postList);
+
   return (
     <ol className="quotes-list">
       {
-        props.postList.filter((data)=>{
-          if(props.search == null)
-            return data
-          else if (data.name.toLowerCase().includes(this.state.search.toLowerCase()) || data.quote.toLowerCase().includes(this.state.search.toLowerCase())){
-            return data
-          }
-        }).map((item, index) =>
+        props.postList.map((item, index) =>
           <Post key={index}
             quote={item.quote}
             author={item.author}
@@ -58,7 +53,8 @@ class App extends React.Component {
     this.state = {
       quote: "",
       author: "",
-      search: "",
+      filterQuote: "",
+      filterAuthor: "",
       items: [{quote: "It is hard to fail, but it is worse never to have tried to succeed.", author: "Theodore Roosevelt"},
         {quote: "Genius is 1% talent and 99% hard work.", author: "Albert Einstein"},
         {quote: "Only put off until tomorrow what you are willing to die having left undone.", author: "Pablo Picasso"}
@@ -76,14 +72,18 @@ class App extends React.Component {
 
   handlechange(event) {
     if (event.target.name === "quote") {
-      this.setState({ search: event.target.value });
+      this.setState({ filterQuote: event.target.value });
     } else if (event.target.name === "author") {
-      this.setState({ search: event.target.value });
+      this.setState({ filterAuthor: event.target.value });
     }
   }
 
   addItem() {
     this.setState({items: [ ...this.state.items, { quote: this.state.quote, author: this.state.author} ].sort((a,b)=>(a.author.localeCompare(b.author))), quote: "", author: ""});
+  }
+
+  addFilter = () => {
+    return this.state.items.filter((item) => item.quote.toLowerCase().includes(this.state.filterQuote.toLowerCase())).filter((item) => item.author.toLowerCase().includes(this.state.filterAuthor.toLowerCase()));    
   }
 
   removeItem(index) {
@@ -95,23 +95,27 @@ class App extends React.Component {
     const searchBox = (
       <div className="search-bar">
           <input 
+            type="text"
             name="quote"
-            value={this.state.quote} 
+            value={this.state.filterQuote} 
             placeholder="Search quote..."
-            onChange={this.handlechange.bind(this)} 
+            onChange={this.handlechange.bind(this)}
+            onClick={this.addFilter()}
           /><br></br>
           <input 
+            type="text"
             name="author" 
-            value={this.state.author} 
+            value={this.state.filterAuthor} 
             placeholder="Search author..."
             onChange={this.handlechange.bind(this)} 
           />
-          <PostList postList={this.state.items}
+          <PostList 
+          postList={this.addFilter()}
           removeItem={this.removeItem.bind(this)}
           />
       </div>
     );
-   
+
     return (
       <div className='quotes-list'>
         <h1>Inspire Today</h1>
